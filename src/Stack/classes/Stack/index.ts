eda49825360f9e7687a4cc08113a934ff1ce4387
Gabriel Silva <gabrieljsilva2301@gmail.com>
2022-02-28 22:25:47 -0300

    fix lint

--- src/Stack/classes/Stack/index.ts
+++ src/Stack/classes/Stack/index.ts
@@ -1,31 +1,31 @@
 export class Stack<T = any> {
-    private items: Array<T> = []
+  private items: Array<T> = [];
 
-    constructor(initialItems: Array<T> = []) {
-      this.items = initialItems;
-    }
+  constructor(initialItems: Array<T> = []) {
+    this.items = initialItems;
+  }
 
-    peek() {
-      return this.items[this.items.length - 1];
-    }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
 
-    push(item: T) {
-      this.items.push(item);
-    }
+  push(item: T) {
+    this.items.push(item);
+  }
 
-    pop() {
-      return this.items.pop();
-    }
+  pop() {
+    return this.items.pop();
+  }
 
-    isEmpty() {
-      return this.items.length === 0;
-    }
+  isEmpty() {
+    return this.items.length === 0;
+  }
 
-    clear() {
-      this.items = [];
-    }
+  clear() {
+    this.items = [];
+  }
 
-    length() {
-      return this.items.length;
-    }
+  length() {
+    return this.items.length;
+  }
 }
