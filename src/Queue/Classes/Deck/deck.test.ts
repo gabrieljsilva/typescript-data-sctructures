c5154d5b6b2ed712242b29bc2951bfbe6d0ad7fc
Gabriel Silva <gabrieljsilva2301@gmail.com>
2022-03-05 01:08:09 -0300

    added unshift and shift methods

--- src/Queue/Classes/Deck/deck.test.ts
+++ src/Queue/Classes/Deck/deck.test.ts
@@ -8,6 +8,29 @@ describe('deck tests', () => {
 
   it('should return true if is empty', () => {
     const deck = new Deck();
-    expect(deck.isEmpty()).toBe(true);
+    expect(deck.isEmpty).toBe(true);
+  });
+
+  it('should add an item in front of a deck if deck is empty', () => {
+    const deck = new Deck();
+    const item = 1;
+    deck.unshift(item);
+    expect(deck.peekFront()).toBe(item);
+  });
+
+  it('should add an item in front of a deck if deck is not empty', () => {
+    const deck = new Deck();
+    const item = 1;
+    deck.unshift(item);
+    expect(deck.peekFront()).toBe(item);
+  });
+
+  it('should add an item in front of a deck if lowestCount > 0', () => {
+    const deck = new Deck();
+    deck.unshift(1);
+    deck.unshift(2);
+    deck.shift();
+    deck.unshift(3);
+    expect(deck.peekFront()).toBe(3);
   });
 });
--- src/Queue/Classes/Deck/index.ts
+++ src/Queue/Classes/Deck/index.ts
@@ -11,7 +11,37 @@ export class Deck<T = any> {
     this.items = {};
   }
 
-  isEmpty() {
+  get isEmpty() {
     return this.count === this.lowestCount;
+  }
+
+  push(item: T) {
+    this.items[this.count++] = item;
+  }
+
+  unshift(item: T) {
+    if (this.isEmpty) {
+      this.push(item);
+    } else if (this.lowestCount > 0) {
+      this.items[--this.lowestCount] = item;
+    } else {
+      for (let i = this.count; i > 0; i--) {
+        this.items[i] = this.items[i - 1];
+      }
+      this.count++;
+      this.lowestCount = 0;
+      this.items[0] = item;
+    }
+  }
+
+  shift() {
+    const item = this.items[this.lowestCount];
+    this.lowestCount++;
+    delete this.items[this.lowestCount];
+    return item;
+  }
+
+  peekFront() {
+    return this.items[this.lowestCount];
   }
 }
