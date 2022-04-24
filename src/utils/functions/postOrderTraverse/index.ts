ce093f1ee21bcf61548b6bb7f6c8d9289401149d
Gabriel Silva <gabrieljsilva2301@gmail.com>
2022-04-24 06:27:25 -0300

    added post order function

--- src/utils/functions/postOrderTraverse/index.ts
+++ src/utils/functions/postOrderTraverse/index.ts
@@ -1,13 +1,13 @@
 import { Node } from '../../../data-structures/BinarySearchTree/Classes/Node';
 import { BinarySearchTreeCallbackFN } from '../../../data-structures/BinarySearchTree/types/index';
 
-export function preOrderTraverse<T = any>(
+export function postOrderTraverse<T = any>(
   node?: Node<T>,
   callback?: BinarySearchTreeCallbackFN<T>,
 ) {
   if (node) {
-    preOrderTraverse(node.left, callback);
-    preOrderTraverse(node.right, callback);
+    postOrderTraverse(node.left, callback);
+    postOrderTraverse(node.right, callback);
     callback?.(node.key);
   }
 }
---
+++ src/utils/functions/postOrderTraverse/postOrderSequence.ts
@@ -0,0 +1,18 @@
+import { BinarySearchTree } from '../../../data-structures/BinarySearchTree/Classes/BinarySearchTree';
+import { postOrderTraverse } from './index';
+
+describe('postOrderTraverse tests', () => {
+  it('should iterate a binary search tree using post-order sequence', () => {
+    const tree = new BinarySearchTree<number>();
+    const postOrderSequence = [0, 1, 3, 5, 4, 2];
+    const insertionSequence = [2, 1, 0, 4, 3, 5];
+
+    insertionSequence.forEach((value) => tree.insert(value));
+
+    let orderSequenceIndex = 0;
+    postOrderTraverse(tree.root, (key) => {
+      expect(key).toBe(postOrderSequence[orderSequenceIndex]);
+      orderSequenceIndex++;
+    });
+  });
+});
