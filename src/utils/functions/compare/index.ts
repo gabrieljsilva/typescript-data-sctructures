6d018c6bba43607300fe7fa43f41202fabc36c57
Gabriel Silva <gabrieljsilva2301@gmail.com>
2022-04-24 05:00:08 -0300

    update Compare enum

--- src/utils/functions/compare/index.ts
+++ src/utils/functions/compare/index.ts
@@ -1,7 +1,7 @@
-export const Compare = {
-  LESS_THAN: -1,
-  BIGGER_THEN: 1,
-};
+export enum Compare {
+  LESS_THAN = 'LESS_THAN',
+  BIGGER_THEN = 'BIGGER_THEN',
+}
 
 export function defaultCompare<T = any>(a: T, b: T) {
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THEN;
